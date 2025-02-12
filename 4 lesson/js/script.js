c5ff885bd8e9e4af6369940f54d383e2da21421c
NoLeak <foggyKING111@gmail.com>
2025-02-12 17:54:53 +0300

    Добавлена папка "4 lesson" c  заданием по операторам. Задание выполнено не полностью. Папка добавлена в репозиторий для работы на стороннем компьютере.

---
+++ 4 lesson/js/script.js
@@ -0,0 +1,44 @@
+//1 task
+
+//1 case
+
+console.log(2 * 2 + 2)
+
+//2 case
+
+let a = 54
+let b = 16
+
+result = ((Math.sin(a) * Math.cos(b)) ** 2)
+
+console.log('Результат = ' + result) // Выведем результат вычислений
+console.log('Округленный результат = ' + Math.round(result)) // Округлим результат до целого числа
+
+//3 case
+
+console.log('Округленный результат = ' + Math.round(((16 * Math.sqrt(13.2 * 71.90)) / (2.4 / 7 ** 4)) + 3 ** Math.sqrt (49)) * 2 ** 7)
+
+// 2 task
+
+let number = 1349
+let d = 235
+let e = 44
+let f = 1816
+
+let last = (number % 10)
+
+if (last = 1 , 3 , 5 , 7 , 9) {
+    console.log('Это НЕЧЕТНОЕ ЧИСЛО')
+}
+else 
+{console.log('Это ЧЕТНОЕ ЧИСЛО')}
+
+let number1 = 44
+
+let last1 = (number1 % 10)
+
+if (last == 1 , 3 , 5 , 7 , 9) {
+    console.log('Это НЕЧЕТНОЕ ЧИСЛО')
+}
+else 
+{console.log('Это ЧЕТНОЕ ЧИСЛО')}
