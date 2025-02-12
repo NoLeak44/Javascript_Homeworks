c188731fd3c3bfcb7994b8ecd53a44b4e566c940
Лилия <foggyKING111@gmail.com>
2025-02-12 19:22:19 +0300

    Добавлены изменения в файл js. Выполнено задание 2

--- 4 lesson/js/script.js
+++ 4 lesson/js/script.js
@@ -1,10 +1,14 @@
 //1 task
 
+console.log('1 task')
+
 //1 case
+console.log('1 case')
 
 console.log(2 * 2 + 2)
 
 //2 case
+console.log('2 case')
 
 let a = 54
 let b = 16
@@ -12,33 +16,72 @@ let b = 16
 result = ((Math.sin(a) * Math.cos(b)) ** 2)
 
 console.log('Результат = ' + result) // Выведем результат вычислений
+
 console.log('Округленный результат = ' + Math.round(result)) // Округлим результат до целого числа
 
 //3 case
+console.log('3 case')
 
-console.log('Округленный результат = ' + Math.round(((16 * Math.sqrt(13.2 * 71.90)) / (2.4 / 7 ** 4)) + 3 ** Math.sqrt (49)) * 2 ** 7)
+result1 = (((16 * Math.sqrt(13.2 * 71.90)) / (2.4 / 7 ** 4)) + 3 ** Math.sqrt(49)) * 2 ** 7
+
+console.log('Результат = ' + result1)
+
+console.log('Округленный результат = ' + Math.round(result1))
 
 // 2 task
 
-let number = 1349
+console.log('2 task')
+
+let c = 1349
 let d = 235
 let e = 44
 let f = 1816
 
-let last = (number % 10)
+console.log('c = ' + c)
 
-if (last = 1 , 3 , 5 , 7 , 9) {
+let last = (c % 2)
+
+console.log('Результат остатка деления = ' + last)
+
+if (last = 1) {
     console.log('Это НЕЧЕТНОЕ ЧИСЛО')
 }
-else 
-{console.log('Это ЧЕТНОЕ ЧИСЛО')}
+else { console.log('Это ЧЕТНОЕ ЧИСЛО') }
 
-let number1 = 44
+console.log('d = ' + d)
 
-let last1 = (number1 % 10)
+let last1 = (d % 2)
 
-if (last == 1 , 3 , 5 , 7 , 9) {
+console.log('Результат остатка деления = ' + last1)
+
+if (last1 == 1) {
     console.log('Это НЕЧЕТНОЕ ЧИСЛО')
 }
-else 
-{console.log('Это ЧЕТНОЕ ЧИСЛО')}
+else { console.log('Это ЧЕТНОЕ ЧИСЛО') }
+
+console.log('e = ' + e)
+
+let last2 = (e % 2)
+
+console.log('Результат остатка деления = ' + last2)
+
+if (last2 == 1) {
+    console.log('Это НЕЧЕТНОЕ ЧИСЛО')
+}
+else { console.log('Это ЧЕТНОЕ ЧИСЛО') }
+
+console.log('f = ' + f)
+
+let last3 = (f % 2)
+
+console.log('Результат остатка деления = ' + last3)
+
+if (last3 == 1) {
+    console.log('Это НЕЧЕТНОЕ ЧИСЛО')
+}
+else { console.log('Это ЧЕТНОЕ ЧИСЛО') }
+
+// 3 task
+
+console.log('3 task')
+
