c09d7f6fceaba34167163cf9e4c981e6471b506a
NoLeak <foggyKING111@gmail.com>
2025-03-21 23:12:25 +0300

    Исправлен урок по функциям, добавлены новые папки с черновиками.

--- Функции/js/script.js
+++ Функции/js/script.js
@@ -2,8 +2,9 @@
 1. Напиши функцию, которая принимает строку и проверяет, является ли она палиндромом. Если да - функция возвращает true, если нет - false;
 */
 function palindromChecker(text) {
-    let normalizedText = text.toLowerCase();
-    let reverse = normalizedText.split("").reverse().join("");
+    const stickyText = text.replace(/[^\p{L}\p{N}]/gu, '');
+    const normalizedText = stickyText.toLowerCase();
+    const reverse = normalizedText.split("").reverse().join("");
     return (normalizedText === reverse);
 }
 
@@ -19,25 +20,30 @@ console.log(palindromChecker(text));
 text = 'Декларация';
 console.log(palindromChecker(text));
 
+text = 'А роза упала на лапу Азора'
+console.log(palindromChecker(text))
+
+//В данном случае я оставил переменную объявленную через let, так как при проверке измененная на const переменная text будет выдавать ошибку.
 
 /*
 2. Напиши функцию, которая принимает строку (предложение) и находит первое самое короткое слово в ней и возвращает его;
 */
 
 function almostShortestWordFinder(longText) {
-    let words = longText.split(/\s+/);
-    let smallestWord = words
+    const words = longText.split(/\s+/);
+    let smallestWord = words[0];
     for (let i = 1; i < words.length; i++) {
         if (words[i].length < smallestWord.length) {
             smallestWord = words[i];
         }
     }
-    return (`Cамое маленькое слово: ${smallestWord}`);
+    return (smallestWord);
+    // В рецензии на работу, указано, что можно улучшить код, возвращая только слово. Я убрал конструкцию с интерполяцией. Надеюсь, я правильно понял указание.
 }
 
 console.log(almostShortestWordFinder('Желание,Ржавый, Семнадцать, Рассвет, Печь, Девять, Добросердечный, Возвращение на родину, Один, Товарный вагон'))
 
-let longText = prompt('Введите Ваше предложение для поиска самого маленького слова')
+const longText = prompt('Введите Ваше предложение для поиска самого маленького слова')
 console.log(almostShortestWordFinder(longText))
 
 
@@ -45,13 +51,22 @@ console.log(almostShortestWordFinder(longText))
 3. Напиши функцию, которая форматирует строку с цифрами в телефонный номер. Пример: createPhoneNumber(123456789) → 8 (123) 456-789;
 */
 
-function createPhoneNumber(phoneNumberArray) {
+function createPhoneNumber(phoneNumberInput) {
+
+    if (typeof phoneNumberInput !== 'string' || phoneNumberInput.length !== 9) {
+        console.log('Некорректные входные данные');
+        return null;
+    }
+
+    const phoneNumberArray = phoneNumberInput.split('');
     const [first, second, third, ...rest] = phoneNumberArray;
     const restPart = rest.join('');
+
     return `8 (${first}${second}${third}) ${restPart.slice(0, 3)}-${restPart.slice(3)}`;
+
 }
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(createPhoneNumber('567891234'));
 
 
 /*
@@ -59,18 +74,28 @@ console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 */
 
 function findSmallestAndLargestItem(someArray) {
-    let minVal = Infinity;
-    let maxVal = -Infinity;
-
-    for (let item of someArray) {
-        if (item < minVal)
-            minVal = item;
-        if (item > maxVal)
-            maxVal = item;
+    if (!Array.isArray(someArray)) {
+        console.log('Тут должен быть массив');
+        return null;
     }
 
-    return (`Min elem is: ${minVal}, Max elem is: ${maxVal}`);
+    if (someArray.length === 0) {
+        console.log('Массив пуст');
+        return null;
+    }
+
+    let minVal = someArray[0];
+    let maxVal = someArray[0];
+
+    for (let item of someArray) {
+        if (item < minVal) minVal = item;
+        if (item > maxVal) maxVal = item;
+    }
+
+
+    return { min: minVal, max: maxVal };
 }
+
 
 const result = findSmallestAndLargestItem([12, 32, 364, 35355, 789797, 16654654, 654, 78798, -14, 635464, -666666666, 65464564]);
 console.log(result);
@@ -88,3 +113,21 @@ function arraySorting(array1) {
 
 console.log(arraySorting([1, 3, 65, 4, 9, 7, 10, 12, -3535, -9, 8, 0]));
 
+function myBubbleSortAttempt(arr) {
+    const arraySize = arr.length;
+
+    for (let j = 0; j < arraySize - 1; ++j) {
+        for (let i = 0; i < arraySize - j - 1; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+
+    console.log(arr);
+}
+
+const arr = [44, -9, -46, 55, 353, -456, 789, 3, 111, 82];
+myBubbleSortAttempt(arr);       
